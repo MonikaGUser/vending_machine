22459a5e9135d15b89b2396927881aecaa633a0c
Monika Gębołyś <monpas@wp.pl>
2018-11-04 13:13:53 +0100

    Testy parametryzowane

--- src/main/java/pl/sdacademy/vending/util/StringUtil.java
+++ src/main/java/pl/sdacademy/vending/util/StringUtil.java
@@ -3,10 +3,41 @@ package pl.sdacademy.vending.util;
 public class StringUtil {
     public static String adjustText(String text, Integer requiredLength) {
         String expandedText = text;
-        while (expandedText.length()<requiredLength){
-            expandedText = " "+ expandedText + " ";
+        while (expandedText.length() < requiredLength) {
+            expandedText = " " + expandedText + " ";
         }
-            return expandedText.substring(0, requiredLength);
+        return expandedText.substring(0, requiredLength);
+    }
+
+    public static String formatMoney(Long amount) {
+        return formatMoneyIntegrals(amount) + "," + formatMoneyDecimals(amount);
+
+    }
+
+    //złotówki
+    private static String formatMoneyIntegrals(Long amount) {
+        String integrals = Long.toString(amount / 100);
+        StringBuilder formatMoney = new StringBuilder();
+        Integer charactersTillLastSpace = 0;
+        for (int charIndex= integrals.length()-1; charIndex>=0; charIndex--){
+            charactersTillLastSpace++;
+           formatMoney = formatMoney.append(integrals.charAt(charIndex));
+            if (charactersTillLastSpace>= 3){
+               formatMoney= formatMoney.append(" ");
+                charactersTillLastSpace =0;
+            }
         }
 
+        return formatMoney.reverse().toString().trim();
+
+    }
+
+    //grosze
+    private static String formatMoneyDecimals(Long amount) {
+        String decimals = Long.toString(amount % 100);
+        if (decimals.length() < 2) {
+            decimals = "0" + decimals;
+        }
+        return decimals;
+    }
 }
--- src/test/java/pl/sdacademy/vending/util/StringUtilTest.java
+++ src/test/java/pl/sdacademy/vending/util/StringUtilTest.java
@@ -1,8 +1,13 @@
 package pl.sdacademy.vending.util;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+
+@RunWith(JUnitParamsRunner.class)
 
 public class StringUtilTest {
 
@@ -48,5 +53,29 @@ public class StringUtilTest {
         //then
         assertEquals("   Ala  ", adjustedText);
     }
+@Test
+    @Parameters
+    public void shouldProperlyFormatMoney(Long amountToFormat, String expectedResult){//given
+        //when
+    String formattedMoney = StringUtil.formatMoney(amountToFormat);
+        //then
+    assertEquals(expectedResult, formattedMoney);
 
+}
+    public Object[] parametersForShouldProperlyFormatMoney(){
+        return new Object[]{
+              new Object[]{123L, "1,23"},
+                new Object[]{0L, "0,00"},
+                new Object[] {5L, "0,05"},
+                new Object[] {12L, "0,12"},
+                new Object[] {1234L, "12,34"},
+                new Object[] {12345L, "123,45"},
+                new Object[] {123456L, "1 234,56"},
+                new Object[] {1234567L, "12 345,67"},
+                new Object[] {12345678L, "123 456,78"},
+                new Object[] {123456789L, "1 234 567,89"},
+                new Object[] {1323455465660845342L, "13 234 554 656 608 453,42"},
+
+        };
+    }
 }
